f1659467c2c1e347cccdc701692ec7f64d25669a
Nate Amack <nate@amack.org>
2019-02-18 18:57:50 -0700

    Revert "remove filler content"
    This reverts commit dd6c7f13d9012eb10f77cda1eebb6e44cc743280.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Nate`,
-    author: `Nate Amack`,
+    title: `Gatsby Starter Blog`,
+    author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://nateamack.com`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `nate_amack`,
+      twitter: `kylemathews`,
     },
   },
   plugins: [
